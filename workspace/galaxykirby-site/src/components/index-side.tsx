dfbf656b09c6f6c477f68e4b3405aea580573b1f
ianp <iepaulhamus@gmail.com>
2026-09-04 00:11:58 -0400

    Updates to index CSS

---
+++ workspace/galaxykirby-site/src/components/index-side.tsx
@@ -0,0 +1,31 @@
+import '../css/index.css'
+
+export default function IndexSide(
+    {image1, image2, image3, image4, 
+            name1, name2, name3, name4}:
+    {image1: string, image2: string, image3: string, image4: string, 
+            name1: string, name2: string, name3: string, name4: string}
+) {
+    return (
+        <>
+            <div className="index-side">
+                <div className="side-img-container">
+                    <img className="side-img" src={image1}/>
+                    <h3 className="index-subheader">{name1}</h3>
+                </div>
+                <div className="side-img-container">
+                    <img className="side-img" src={image2}/>
+                    <h3 className="index-subheader">{name2}</h3>
+                </div>
+                <div className="side-img-container">
+                    <img className="side-img" src={image3}/>
+                    <h3 className="index-subheader">{name3}</h3>
+                </div>
+                <div className="side-img-container">
+                    <img className="side-img" src={image4}/>
+                    <h3 className="index-subheader">{name4}</h3>
+                </div>
+            </div>
+        </>
+    )
+}
