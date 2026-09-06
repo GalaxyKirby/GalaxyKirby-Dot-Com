14bda2162e986e302df3041ba3a12b4fd75a8c4c
ianp <iepaulhamus@gmail.com>
2026-09-06 14:31:41 -0400

    Cleaning up the main page

--- workspace/galaxykirby-site/src/components/index-center.tsx
+++ workspace/galaxykirby-site/src/components/index-center.tsx
@@ -5,14 +5,11 @@ export default function IndexCenter() {
         <>
             <div id="index-center">
                 <h2 className="index-title">I'm Chai, Nice To Meet You</h2>
-                <br />
                 <p className="bio-text">I’m a nerd and deer who is always looking to better myself. 
                     If I’m not frolicking, I’m typing away in VS Code 
-                    or absorbed in the fantasy world of a video game
+                    or absorbed in the fantasy world of a video game.
                 </p>
-                <br />
                 <hr className="index-hr" />
-                <br />
                 <p className="index-subtitle">My Numerous Hobbies</p>
                 <div id="index-hobbies-container">
                     <div className="hobbies-row">
--- workspace/galaxykirby-site/src/pages/index.tsx
+++ workspace/galaxykirby-site/src/pages/index.tsx
@@ -7,7 +7,12 @@ import IndexSide from '../components/index-side'
 import Metaphor from '../assets/Metaphor-Icon.png'
 import PokemonBW from '../assets/Pokemon-BW-Icon.png'
 import Splatoon3 from '../assets/Splatoon-3.png'
+import Reload from '../assets/Persona-3-Reload-Icon.webp'
 
+import Arcanine from '../assets/Arcanine-Portrait.png'
+import Espeon from '../assets/Espeon-Portrait.png'
+import Bulbasaur from '../assets/Bulbasaur.png'
+import Dragonite from '../assets/Dragonite-Portrait.png'
 
 export default function Index() {
     return (
@@ -16,7 +21,23 @@ export default function Index() {
                 <Hero />
                 <br />
                 <div id="index-content">
-                    <IndexSide image1=""/>
+                    <IndexSide 
+                        image1={Metaphor} image2={PokemonBW}
+                        image3={Splatoon3} image4={Reload}
+
+                        name1="Metaphor Refantazio" name2="Pokemon Black/White"
+                        name3="Splatoon 3" name4="Persona 3 Reload"
+                    />
+
+                    <IndexCenter />
+
+                    <IndexSide
+                        image1={Arcanine} image2={Espeon}
+                        image3={Bulbasaur} image4={Dragonite}
+
+                        name1="Arcanine" name2="Espeon"
+                        name3="Bulbasaur" name4="Dragonite"
+                    />
                 </div>
             </body>
         </>
